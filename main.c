e5f87f4a60cd7f06d75e2b29c1989be0d524f26b
Kush Goswami <kushvgoswami84@gmail.com>
2026-01-19 18:14:37 -0500

    Add gradient interpolation

--- main.c
+++ main.c
@@ -9,9 +9,15 @@ void create_image() {
     int x;
     int y;
 
+    RGB curr_color = {127, 178, 255};
+
     for (y = 0; y < 256; y++) {
+        curr_color.r = 127 + (255 - 127) * y / 255.0;
+        curr_color.g = 178 + (255 - 178) * y / 255.0;
+        curr_color.b = 255 + (255 - 255) * y / 255.0;
+
         for (x = 0; x < 256; x++) {
-            image[y][x] = (RGB){184, 55, 46};
+            image[y][x] = curr_color;
         }
     }
 
