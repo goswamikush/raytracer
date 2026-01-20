d0a07255d996c3b9ce09254b4a45a3610de8453b
Kush Goswami <kushvgoswami84@gmail.com>
2026-01-19 22:31:53 -0500

    Refactored tests

--- tests/test_vec3.c
+++ tests/test_vec3.c
@@ -13,6 +13,7 @@ typedef struct {
 
 bool addition_test(BASE_TEST_CASE);
 bool subtraction_test(BASE_TEST_CASE);
+int base_test_runner(bool (*func)(BASE_TEST_CASE), BASE_TEST_CASE[], int);
 bool is_equal(vec3_t, vec3_t);
 
 int main() {
@@ -22,77 +23,61 @@ int main() {
     // Setup addition test cases
     BASE_TEST_CASE addition_test_cases[2];
 
-    BASE_TEST_CASE simple = {
+    BASE_TEST_CASE add_t1 = {
         {1, 5, 4},
         {2, 6, 8},
         {3, 11, 12}
     };
 
-    addition_test_cases[0] = simple;
-
-    BASE_TEST_CASE complex = {
+    BASE_TEST_CASE add_t2 = {
         {1.022, 5.891, 4.56},
         {2.2, 6.12, 8.97},
         {3.222, 12.0110, 13.53}
     };
 
-    addition_test_cases[1] = complex;
+    addition_test_cases[0] = add_t1;
+    addition_test_cases[1] = add_t2;
 
-    // Loop through all test cases
-    int num_cases = sizeof(addition_test_cases) / sizeof(addition_test_cases[0]);
-    int i;
-    bool passed;
+    int num_add_cases = sizeof(addition_test_cases) / sizeof(addition_test_cases[0]);
 
-    printf("Running addition tests");
-    for (i = 0; i < num_cases; i++) {
-        passed = addition_test(addition_test_cases[i]);
-        if (passed) {
-            printf("Passed test case %d\n", i);
-            num_passed++;
-        } else {
-            printf("Passed test case %d\n", i);
-            num_failed++;
-        }
-    };
+    printf("Running addition tests\n");
+    int add_fail = base_test_runner(*addition_test, addition_test_cases, num_add_cases);
 
+    num_failed += add_fail;
+    num_passed += num_add_cases - add_fail;
+    
     // Setup subtraction test cases
     BASE_TEST_CASE subtraction_test_cases[3];
 
-    BASE_TEST_CASE t1 = {
+    BASE_TEST_CASE sub_t1 = {
         {9, 5, 4},
         {6, 3, 2},
         {3, 2, 2},
     };
 
-    subtraction_test_cases[0] = t1;
-
-    BASE_TEST_CASE t2 = {
+    BASE_TEST_CASE sub_t2 = {
         {3, 1, 4},
         {6, 3, 2},
         {-3, -2, 2},
     };
 
-    subtraction_test_cases[1] = t2;
-
-    BASE_TEST_CASE t3 = {
+    BASE_TEST_CASE sub_t3 = {
         {1.022, 6.891, 4.56},
         {2.2, 5.12, 8.97},
         {-1.178, 1.771, -4.41}
     };
 
-    subtraction_test_cases[2] = t3;
+    subtraction_test_cases[0] = sub_t1;
+    subtraction_test_cases[1] = sub_t2;
+    subtraction_test_cases[2] = sub_t3;
 
-    printf("Running subtraction tests");
-    for (i = 0; i < num_cases; i++) {
-        passed = subtraction_test(subtraction_test_cases[i]);
-        if (passed) {
-            printf("Passed test case %d\n", i);
-            num_passed++;
-        } else {
-            printf("Passed test case %d\n", i);
-            num_failed++;
-        }
-    };
+    int num_sub_cases = sizeof(subtraction_test_cases) / sizeof(subtraction_test_cases[0]);
+
+    printf("Running subtraction tests\n");
+    int sub_fail = base_test_runner(*subtraction_test, subtraction_test_cases, num_sub_cases);
+
+    num_failed += sub_fail;
+    num_passed += num_sub_cases - sub_fail;
 
     printf("Total passed: %d\n", num_passed);
     printf("Total failes: %d\n", num_failed);
@@ -106,7 +91,7 @@ bool addition_test(BASE_TEST_CASE test_case) {
 
     bool passed = is_equal(res, test_case.ground_truth);
 
-    return true;
+    return passed;
 }
 
 // Test subtraction
@@ -115,8 +100,27 @@ bool subtraction_test(BASE_TEST_CASE test_case) {
 
     bool passed = is_equal(res, test_case.ground_truth);
 
-    return true;
+    return passed;
 }
+
+int base_test_runner(bool (*func)(BASE_TEST_CASE), BASE_TEST_CASE test_cases[], int num_cases) {
+    int num_passed = 0;
+    int num_failed = 0;
+
+    int i;
+    for (i = 0; i < num_cases; i++) {
+        bool passed = func(test_cases[i]);
+        if (passed) {
+            printf("Passed test case %d\n", i);
+            num_passed++;
+        } else {
+            printf("Passed test case %d\n", i);
+            num_failed++;
+        }
+    }
+
+    return num_failed;
+};
 
 // Equality function
 bool is_equal(vec3_t v1, vec3_t v2) {
